e05ae2c1f553ebd4c53d287fe310cfd78fac64cb
Khadeja Iqbal <khadejaiqbal@sabanciuniv.edu>
2021-04-10 19:09:20 +0500

    styles-utils

---
+++ socialapp310/lib/utils/styles.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:socialapp310/utils/color.dart';
+
+
+
+final kHeadingTextStyle = TextStyle(
+  color: AppColors.darkpurple,
+  fontWeight: FontWeight.w900,
+  fontSize: 30.0,
+  letterSpacing: -0.7,
+);
+
+final kButtonLightTextStyle = TextStyle(
+  color: AppColors.primarypurple,
+  fontSize: 20.0,
+  letterSpacing: -0.7,
+);
+
+final kLabelLightTextStyle = TextStyle(
+  color: AppColors.darkgreyblack,
+  fontSize: 13.0,
+);
+
+final kButtonDarkTextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 20.0,
+  letterSpacing: -0.7,
+);
+
+final kButtonDarksmallTextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 18.0,
+  letterSpacing: -0.7,
+);
+
+
+final kAppBarTitleTextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 24.0,
+  fontWeight: FontWeight.w600,
+  letterSpacing: -0.7,
+);
+
+final kTitle = TextStyle(
+  color: AppColors.darkpurple,
+  fontSize: 18.0,
+  fontWeight: FontWeight.w400,
+  letterSpacing: -0.7,
+);
